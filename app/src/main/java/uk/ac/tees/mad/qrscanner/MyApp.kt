cf5369fb799e3d345a99b754f1822b6f3973c9ab
Nagasai <qrscanner48@gmail.com>
2025-03-04 12:00:00 +0530

    Project setup

---
+++ app/src/main/java/uk/ac/tees/mad/qrscanner/MyApp.kt
@@ -0,0 +1,7 @@
+package uk.ac.tees.mad.qrscanner
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApp: Application()
---
+++ app/src/main/java/uk/ac/tees/mad/qrscanner/navigation/AppNavigation.kt
@@ -0,0 +1,8 @@
+package uk.ac.tees.mad.qrscanner.navigation
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun AppNavigation() {
+
+}
